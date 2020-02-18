878c5d47d825e1a426defd727a784738882a2645
Alexandr-01 <derkachev01@list.ru>
2020-02-18 13:54:29 +0300

    ДЗ исправлено

--- Lessons/src/main/java/ru/java/courses/Lesson10_CollectionsLists.java
+++ Lessons/src/main/java/ru/java/courses/Lesson10_CollectionsLists.java
@@ -1,9 +1,9 @@
 package ru.java.courses;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Lesson10_CollectionsLists {
 
@@ -19,13 +19,7 @@ public class Lesson10_CollectionsLists {
      * Удалять элементы из итерируемого списка нельзя - выпадет исключение
      */
     public static List<String> task1(List<Integer> source) {
-        List<Integer> res = new ArrayList<Integer>();
-        for (Integer integer : source) {
-            if (!(res.contains(integer))) {
-                res.add(integer);
-            }
-        }
-        Collections.sort(res);
+        Set<Integer> res = new TreeSet<>(source);
         List<String> resStr = new ArrayList<String>();
         for (Integer k : res) {
             resStr.add(k.toString());
@@ -43,14 +37,11 @@ public class Lesson10_CollectionsLists {
      * Подсказка: на входе может быть любое количество чисел
      */
     public static List<Integer> task2(Integer... array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                array[i]++;
-            }
-        }
         List<Integer> res = new ArrayList<Integer>();
         if (array.length >= 4) {
-            res.addAll(Arrays.asList(array).subList(3, Math.min(array.length, 8)));
+            for (int i = 3; i < Math.min(array.length, 8); i++) {
+                res.add(array[i] % 2 == 0 ? ++array[i] : array[i]);
+            }
         }
         return res;
     }
